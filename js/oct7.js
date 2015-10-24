980a44c78b348aece60b6c46de00a3b19576e84d
Tyler Larson <lars4629@umn.edu>
2015-10-24 16:06:41 -0500

    Data of video 3 and 4

--- js/oct7.js
+++ js/oct7.js
@@ -1,16 +1,16 @@
-var videos = [ "v1-bike_rack_covers", "v2-abi" ];
+var videos = [ "v1-bike_rack_covers", "v2-abi", "v3-vacuum_unclogging", "v4-vacuum_bags" ];
 
-var names = [ "Bike Rack Covers", "Mastering ABI" ];
+var names = [ "Bike Rack Covers", "Mastering ABI", "Vacuum Unclogging", "Vacuum Bag Changing" ];
 
-var lengths = [ "1:15", "4:30" ];
+var lengths = [ "1:15", "4:30", "4:40", "0:30" ];
 
-var views = [ "25", "12" ];
+var views = [ "25", "12", "1", "19" ];
 
-var usefulness = [ "3.1", "2.5" ];
+var usefulness = [ "3.1", "2.5", "3.7", "4.1"];
 
-var description = [ "This video explains how to properly fold bike rack covers.", "This video provides a quick explanation of everything you need to know about ABI and how to do those things." ];
+var description = [ "This video explains how to properly fold bike rack covers.", "This video provides a quick explanation of everything you need to know about ABI and how to do those things.", "This is a video that shows you how to unclog a vacuum, make sure it is in good working condition, how to change a vacuum bag and provide other handy tips.", "This is a video that shows you how to change a vacuum bag." ];
 
-var related = [ [ "v2-abi", "v1-bike_rack_covers", "v2-abi" ], [ "v1-bike_rack_covers", "v2-abi", "v1-bike_rack_covers" ] ];
+var related = [ [ "v2-abi", "v3-vacuum_unclogging", "v4-vacuum_bags" ], [ "v1-bike_rack_covers", "v3-vacuum_unclogging", "v4-vacuum_bags" ], [ "v2-abi", "v1-bike_rack_covers", "v4-vacuum_bags" ], [ "v2-abi", "v3-vacuum_unclogging", "v1-bike_rack_covers" ] ];
 
 function video( x ) {
    var current = videos.indexOf( x );
